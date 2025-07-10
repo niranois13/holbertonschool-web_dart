b3e35485a4adccaaf2949eb171418aff73864641
niranois13 <8784@holbertonstudents.com>
2025-07-10 15:52:47 +0200

    Task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,29 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String response = await fetchUserData();
+    Map<String, dynamic> data = jsonDecode(response);
+    String userName = data['username'];
+    return ('Hello $userName');
+  } catch (error) {
+    return ('error caught: $error');
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool response = await checkCredentials();
+    switch(response) {
+      case true:
+        print('There is a user: $response');
+        return(greetUser());
+      case false:
+        print('There is a user: $response');
+        return ('Wrong credentials');
+    }
+  } catch (error) {
+    return ('error caught: $error');
+  }
+}
